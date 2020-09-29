c2f40d363acac444176b20ff5c6fc6af1d8e734f
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2020-09-28 23:11:23 -0300

    listando produtos na tela e contando quantos existem

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,6 +3,10 @@ import api from '../../services/api';
 
 
 export default class Main extends Component {
+    state = {
+        products: []
+    };
+    
     componentDidMount(){
         this.loadProducts();
     }
@@ -10,11 +14,17 @@ export default class Main extends Component {
     loadProducts = async () =>{
         const response = await api.get('/products');
 
-        console.log(response);
+        //console.log(response.data.docs); testando se o response deu certo
+        this.setState({ products: response.data.docs });
     };
 
 
     render(){
-        return <h1>Hello RocketSeat</h1>
+        return <div className="product-list">
+            <h1>Contagem de produtos: {this.state.products.length}</h1>
+            {this.state.products.map(product => (
+                <h2 key={product._id}>{product.title}</h2> //A key é algo que o react pede após realizarmos um map, para adicionarmos a cada produto
+            ))}
+        </div>
     }
 }
