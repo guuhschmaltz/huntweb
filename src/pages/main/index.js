d2a2c33d4705d12427f194de9ecc8a625e0ac757
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2020-09-29 23:30:47 -0300

    adicionado navegação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,30 +1,66 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import './styles.css';
+
 
 export default class Main extends Component {
     state = {
-        products: []
+        products: [],
+        productInfo: {},
+        page: 1,
     };
     
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = async () =>{
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) =>{
+        const response = await api.get(`/products?page=${page}`);
+
+        const { docs, ... productInfo } = response.data;
 
         //console.log(response.data.docs); testando se o response deu certo
-        this.setState({ products: response.data.docs });
+        this.setState({ products: docs, productInfo, page });
     };
 
 
+prevPage = () => {
+    const { page, productInfo} = this.state;
+    if (page === 1) return;
+
+    const pageNumber = page-1;
+
+    this.loadProducts(pageNumber);
+}
+nextPage = () => {
+    const { page, productInfo} = this.state;
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page+1;
+
+    this.loadProducts(pageNumber);
+}
+
+
+//A key é algo que o react pede após realizarmos um map, para adicionarmos a cada produto
     render(){
+        const { products, page , productInfo}  = this.state;
+
         return <div className="product-list">
             <h1>Contagem de produtos: {this.state.products.length}</h1>
-            {this.state.products.map(product => (
-                <h2 key={product._id}>{product.title}</h2> //A key é algo que o react pede após realizarmos um map, para adicionarmos a cada produto
+            {products.map(product => (
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href="">Acessar</a>
+                </article>
             ))}
+            <div className="actions">
+                <button disabled= {page === 1} onClick={this.prevPage}>Anterior</button>
+                <button disabled= {page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+            </div>
         </div>
     }
 }
